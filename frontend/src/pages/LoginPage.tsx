068446631d11a3528113890ca40c57694e7bbcdc
Sean Lee <90493212+seansjlee@users.noreply.github.com>
2025-05-13 21:24:44 +0900

    style: update default setting

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -13,11 +13,11 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gray-900 text-white">
-      <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md">
-        <div className="text-center mb-6">
+    <div className="w-full min-h-screen overflow-x-hidden bg-blue-50">
+      <div className="p-8 bg-white rounded-lg shadow-lg ">
+        <div className="mb-6 text-center">
           <p className="text-4xl font-bold text-gray-800">Repkit</p>
-          <p className="text-xl text-gray-600 mt-2">
+          <p className="mt-2 text-xl text-gray-600">
             Design your workout toolkit.
           </p>
         </div>
@@ -27,23 +27,23 @@ const LoginPage = () => {
             placeholder="Enter your username."
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            className="px-4 py-3 border rounded-md border-gray-300 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="px-4 py-3 text-gray-800 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <Input
             type="password"
             placeholder="Enter your password."
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="px-4 py-3 border rounded-md border-gray-300 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="px-4 py-3 text-gray-800 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <button
             type="submit"
-            className="px-4 py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="px-4 py-3 font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
             Log in
           </button>
         </form>
-        <div className="text-center mt-6 text-sm text-gray-500">
+        <div className="mt-6 text-sm text-center text-gray-500">
           <p>&copy; 2025 Sean Lee. All rights reserved.</p>
         </div>
       </div>
