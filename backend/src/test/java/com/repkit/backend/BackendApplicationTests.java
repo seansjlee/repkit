06839f948d8b6007fe81ec2ef7a0c8e6916ed9b6
Sean Lee <90493212+seansjlee@users.noreply.github.com>
2025-06-06 19:51:53 +0900

    chore(backend): initial commit

---
+++ backend/src/test/java/com/repkit/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repkit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
